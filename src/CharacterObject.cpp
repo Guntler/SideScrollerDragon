d565a98d8a267777bbb5db2d1953bd156e827faf
Diogo Eiras <eiras@outlook.com>
2014-02-12 19:20:43 +0000

    Delete unused files

--- src/CharacterObject.cpp
+++
@@ -1,6 +0,0 @@
-#include "CharacterObject.h"
-
-CharacterObject::CharacterObject(){}
-int CharacterObject::getHP(){return this->hp;}
-bool CharacterObject::isAffected(){return this->affected;}
-bool CharacterObject::hasAttack(){return this->attack;}
--- src/MovingObject.cpp
+++
@@ -1,4 +0,0 @@
-#include "MovingObject.h"
-
-int * MovingObject::getCollisionPts(){return this->collisionPoints;}
-bool MovingObject::hasGravity(){return this->gravity;}
