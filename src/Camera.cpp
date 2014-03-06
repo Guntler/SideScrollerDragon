1c9c57700c42d215e01104996e4bba8a4fa9c1b0
Diogo Eiras <eiras@outlook.com>
2014-03-06 20:35:18 +0000

    Camera control improvements

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -7,4 +7,10 @@ void Camera::scrollCamera(GameObject * obj, sf::View * view){
 	if (view->getCenter().x > obj->getPosition().x && obj->getPosition().x > 80){
 		view->setCenter(view->getCenter().x + (obj->getPosition().x - view->getCenter().x + 15), view->getCenter().y);
 	}
+	if (obj->getPosition().y - 15 > view->getCenter().y){ // && obj->getPosition().y < MAX - 80
+		view->setCenter(view->getCenter().x, view->getCenter().y + (obj->getPosition().y - 15 - view->getCenter().y));
+	}
+	if (obj->getPosition().y < view->getCenter().y && obj->getPosition().y >= 80){
+		view->setCenter(view->getCenter().x, view->getCenter().y - (view->getCenter().y - obj->getPosition().y));
+	}
 }
